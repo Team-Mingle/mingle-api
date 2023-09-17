a3ae219aa5fac0f63891a3ba8b56f5e56c7c220b
Hyeonwoo <leojung0909@gmail.com>
2023-09-17 17:29:59 +0900

    :tada: initial commit

---
+++ src/test/java/community/mingle/api/MingleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package community.mingle.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MingleApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
