0ff9353df4c7c7b2618b081c320d41096e4c6494
taehyun <tanggu01@connect.hku.hk>
2023-10-09 21:15:06 +0900

    feat: 토큰 관련 로직 추가

---
+++ src/main/java/community/mingle/api/domain/member/TokenService.java
@@ -0,0 +1,25 @@
+package community.mingle.api.domain.member;
+
+import community.mingle.api.domain.member.entity.Member;
+import community.mingle.api.enums.MemberRole;
+import community.mingle.api.security.component.TokenHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TokenService {
+
+    private final TokenHandler tokenHandler;
+
+    public TokenResult createTokens(Member member, String hashedEmail) {
+        Long memberId = member.getId();
+        MemberRole memberRole = member.getRole();
+        String accessToken = tokenHandler.createAccessToken(memberId, memberRole);
+        String refreshToken = tokenHandler.createRefreshToken(memberId, memberRole, hashedEmail);
+        return new TokenResult(accessToken, refreshToken);
+    }
+
+    record TokenResult(String accessToken, String refreshToken) {
+    }
+}
--- src/main/java/community/mingle/api/security/component/TokenHandler.java
+++ src/main/java/community/mingle/api/security/component/TokenHandler.java
@@ -3,12 +3,16 @@ package community.mingle.api.security.component;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import community.mingle.api.enums.MemberRole;
+import community.mingle.api.global.exception.CustomException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
+
+import static community.mingle.api.global.exception.ErrorCode.FAILED_TO_CREATEJWT;
 
 @Component
 @AllArgsConstructor
@@ -18,19 +22,47 @@ public class TokenHandler {
 
     //TODO RefreshToken도 redis를 뺄까?
     public String createAccessToken(Long memberId, MemberRole memberRole) {
-        return JWT.create()
-                .withClaim("memberId", memberId)
-                .withClaim("memberRole", memberRole.toString())
-                .withExpiresAt(
-                        Date.from(
-                                LocalDateTime
-                                        .now()
-                                        .plusMinutes(30)
-                                        .atZone(ZoneId.of("Asia/Seoul"))
-                                        .toInstant()
-                        )
-                )
-                .sign(tokenAlgorithm);
+        try {
+            return JWT.create()
+                    .withClaim("memberId", memberId)
+                    .withClaim("memberRole", memberRole.toString())
+                    .withExpiresAt(
+                            Date.from(
+                                    LocalDateTime
+                                            .now()
+                                            .plusMinutes(30)
+                                            .atZone(ZoneId.of("Asia/Seoul"))
+                                            .toInstant()
+                            )
+                    )
+                    .sign(tokenAlgorithm);
+        } catch (Exception e) {
+            throw new CustomException(FAILED_TO_CREATEJWT);
+        }
     }
+
+
+    public String createRefreshToken(Long memberId, MemberRole memberRole, String email) {
+        try {
+            return JWT.create()
+                    .withClaim("memberId", memberId)
+                    .withClaim("memberRole", memberRole.toString())
+                    .withExpiresAt(
+                            Date.from(
+                                    LocalDateTime
+                                            .now()
+                                            .plusDays(30)
+                                            .atZone(ZoneId.of("Asia/Seoul"))
+                                            .toInstant()
+                            )
+                    )
+                    .sign(tokenAlgorithm);
+            //TODO Refresh token 저장 위한 테이블 생성
+            //        redisService.setValues(email, refreshToken, Duration.ofDays(refreshTokenMaxAgeSeconds));
+        } catch (Exception e) {
+            throw new CustomException(FAILED_TO_CREATEJWT);
+        }
+    }
+
 
 }
