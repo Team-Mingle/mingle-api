b1db422a6892e630246c6218b9c3ce6de8717b6c
mj326 <minjun32622@gmail.com>
2023-10-22 04:36:31 +0800

    added List<CountryResponse>

--- src/main/java/community/mingle/api/domain/auth/controller/Authcontroller.java
+++ src/main/java/community/mingle/api/domain/auth/controller/Authcontroller.java
@@ -1,6 +1,7 @@
 package community.mingle.api.domain.auth.controller;
 
 
+import community.mingle.api.domain.auth.controller.response.CountryResponse;
 import lombok.RequiredArgsConstructor;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
@@ -12,8 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth")
-
-
 public class Authcontroller {
 
     private final CountryService countryService;
@@ -21,9 +20,9 @@ public class Authcontroller {
     /**
      * 국가 리스트 api
      */
-    @GetMapping("countries")
-    public ResponseEntity<List<String>> getCountries() {
-        List<String> countries = countryService.getCountries();
+    @GetMapping("/countries")
+    public ResponseEntity<List<CountryResponse>> getCountries() {
+        List<CountryResponse> countries = countryService.getCountries();
         return ResponseEntity.ok().body(countries);
     }
 }
--- src/main/java/community/mingle/api/domain/member/service/CountryService.java
+++ src/main/java/community/mingle/api/domain/member/service/CountryService.java
@@ -2,23 +2,24 @@ package community.mingle.api.domain.member.service;
 
 import java.util.List;
 
+import community.mingle.api.domain.auth.controller.response.CountryResponse;
 import community.mingle.api.domain.member.entity.Country;
 import community.mingle.api.domain.member.repository.CountryRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 import java.util.stream.Collectors;
-
 @Service
 @RequiredArgsConstructor
 public class CountryService {
     private final CountryRepository countryRepository;
 
-    public List<String> getCountries() {
+    public List<CountryResponse> getCountries() {
         List<Country> countries = countryRepository.findAll();
-        return countries.stream().map(Country::getCountry).collect(Collectors.toList());
+        return countries.stream()
+                .map(country -> new CountryResponse(country.getCountry()))
+                .collect(Collectors.toList());
     }
+
+
 
 }
