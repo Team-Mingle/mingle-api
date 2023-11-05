abc407a1e79604cb8b17e97d10cd2d101e0e6fc4
Hyeonwoo <leojung0909@gmail.com>
2023-11-05 20:51:07 +0900

    :sparkles: add QueryDslConfiguration

---
+++ src/main/java/community/mingle/api/domain/member/repository/CountryQueryRepository.java
@@ -0,0 +1,11 @@
+package community.mingle.api.domain.member.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class CountryQueryRepository {
+
+}
