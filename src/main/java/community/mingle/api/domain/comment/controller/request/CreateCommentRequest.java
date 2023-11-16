6bffbbfd0156afe9f21fbaddb07dff7e6c9733c0
Hyeonwoo <leojung0909@gmail.com>
2023-11-16 19:02:34 +0900

    :wastebasket: remove unused import statement

--- src/main/java/community/mingle/api/domain/comment/controller/request/CreateCommentRequest.java
+++ src/main/java/community/mingle/api/domain/comment/controller/request/CreateCommentRequest.java
@@ -1,9 +1,7 @@
 package community.mingle.api.domain.comment.controller.request;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import lombok.Data;
 
 public record CreateCommentRequest(
         @NotNull(message = "postId를 입력해주세요")
