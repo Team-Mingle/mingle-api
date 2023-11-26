fb1f18395fe3904948c0f72ac4da743be1546aa3
HYEONWOO JUNG <leojung0909@gmail.com>
2023-11-26 16:32:36 +0900

    :sparkles: add validation to request object

--- src/main/java/community/mingle/api/domain/course/controller/request/CreatePersonalCourseRequest.java
+++ src/main/java/community/mingle/api/domain/course/controller/request/CreatePersonalCourseRequest.java
@@ -1,10 +1,12 @@
 package community.mingle.api.domain.course.controller.request;
 
 import community.mingle.api.dto.course.CourseTimeDto;
+import jakarta.validation.constraints.NotBlank;
 
 import java.util.List;
 
 public record CreatePersonalCourseRequest(
+        @NotBlank(message = "강의명을 입력해주세요.")
         String name,
         List<CourseTimeDto> courseTimeDtoList,
         String courseCode,
