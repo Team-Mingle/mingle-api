a36fd0207a38111aca301cee24d52f81fa971443
HYEONWOO JUNG <leojung0909@gmail.com>
2023-11-26 13:34:35 +0900

    :wastebasket: remove unused import statement

--- src/main/java/community/mingle/api/domain/course/entity/CourseTimetable.java
+++ src/main/java/community/mingle/api/domain/course/entity/CourseTimetable.java
@@ -3,12 +3,13 @@ package community.mingle.api.domain.course.entity;
 import community.mingle.api.entitybase.AuditLoggingBase;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
-
-import java.time.Instant;
 
 @Getter
 @Entity
