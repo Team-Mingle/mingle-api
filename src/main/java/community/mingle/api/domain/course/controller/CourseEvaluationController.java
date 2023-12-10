f1552a864eea103968f078bd163508ab51465f0d
HYEONWOO JUNG <leojung0909@gmail.com>
2023-12-10 09:41:33 +0900

    :memo: add controller swagger description

--- src/main/java/community/mingle/api/domain/course/controller/CourseEvaluationController.java
+++ src/main/java/community/mingle/api/domain/course/controller/CourseEvaluationController.java
@@ -3,13 +3,13 @@ package community.mingle.api.domain.course.controller;
 import community.mingle.api.domain.course.controller.request.CreateCourseEvaluationRequest;
 import community.mingle.api.domain.course.facade.CourseEvaluationFacade;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@Tag(name = "Course Evaluation Controller", description = "강의평가 관련 API")
 @RestController
 @RequestMapping("/course-evaluation")
 @RequiredArgsConstructor
