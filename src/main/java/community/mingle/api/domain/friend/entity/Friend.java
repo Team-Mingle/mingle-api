566a9b86bedfe03340ad673b312f3239c4dc7c86
HYEONWOO JUNG <leojung0909@gmail.com>
2023-12-12 23:13:20 +0900

    :card_file_box: add friend and friend_code table and its entity

---
+++ src/main/java/community/mingle/api/domain/friend/entity/Friend.java
@@ -0,0 +1,42 @@
+package community.mingle.api.domain.friend.entity;
+
+import community.mingle.api.domain.member.entity.Member;
+import community.mingle.api.entitybase.AuditLoggingBase;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@Where(clause = "deleted_at IS NULL")
+@SQLDelete(sql = "UPDATE friend SET deleted_at = CURRENT_TIMESTAMP WHERE id = ?")
+@AllArgsConstructor
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "friend")
+public class Friend extends AuditLoggingBase {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "friend_id", nullable = false)
+    private Member friend;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+}
