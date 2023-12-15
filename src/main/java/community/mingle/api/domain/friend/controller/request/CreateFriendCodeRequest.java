f67ff801652671e4e853a1b9bd56232af1be6ac4
HYEONWOO JUNG <leojung0909@gmail.com>
2023-12-15 18:59:15 +0900

    :memo: add request validation with message

--- src/main/java/community/mingle/api/domain/friend/controller/request/CreateFriendCodeRequest.java
+++ src/main/java/community/mingle/api/domain/friend/controller/request/CreateFriendCodeRequest.java
@@ -1,6 +1,9 @@
 package community.mingle.api.domain.friend.controller.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record CreateFriendCodeRequest(
+        @NotBlank(message = "친구에게 보여질 이름을 입력해주세요.")
         String defaultMemberName
 ) {
 }
