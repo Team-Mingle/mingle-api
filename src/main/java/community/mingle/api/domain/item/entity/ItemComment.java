7b030df4724825d299d58f6788f355b45f6ae5e3
taehyun <tanggu01@connect.hku.hk>
2024-01-04 01:36:34 -0500

    feat: /item 관련 table & entity 추가 마무리

---
+++ src/main/java/community/mingle/api/domain/item/entity/ItemComment.java
@@ -0,0 +1,73 @@
+package community.mingle.api.domain.item.entity;
+
+import community.mingle.api.domain.member.entity.Member;
+import community.mingle.api.entitybase.AuditLoggingBase;
+import community.mingle.api.enums.ContentStatusType;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Table(name = "item_comment")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Where(clause = "deleted_at IS NULL")
+@SQLDelete(sql = "UPDATE item_comment SET deleted_at = CURRENT_TIMESTAMP, status = 'INACTIVE' WHERE id = ?")
+public class ItemComment extends AuditLoggingBase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "item_id", nullable = false)
+    private Item item;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @NotNull
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Column(name = "parent_comment_id")
+    private Long parentCommentId;
+
+    @Column(name = "mention_id")
+    private Long mentionId;
+
+    @OneToMany(mappedBy = "itemComment")
+    private List<ItemCommentLike> itemCommentLikes = new ArrayList<>();
+
+    @NotNull
+    @Column(name = "anonymous", nullable = false)
+    private Boolean anonymous = false;
+
+    @Column(name = "anonymous_id")
+    private Long anonymousId;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    @NotNull
+    @Column(name = "status", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private ContentStatusType status;
+
+    public void deleteItemComment() {
+        this.deletedAt = LocalDateTime.now();
+        this.status = ContentStatusType.INACTIVE;
+    }
+}
