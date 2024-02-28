0b8eb6f1e8ea70d80b945897efecb2d75beeda73
taehyun <tanggu01@connect.hku.hk>
2024-02-28 00:17:33 -0500

    fix: FCM 푸시알림 로직 기존 서버 로직으로 원복

---
+++ src/main/java/community/mingle/api/global/firebase/FcmMessage.java
@@ -0,0 +1,41 @@
+package community.mingle.api.global.firebase;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@AllArgsConstructor
+@Getter
+public class FcmMessage {
+    private boolean validate_only;
+    private Message message;
+
+    @Builder
+    @AllArgsConstructor
+    @Getter
+    public static class Message {
+        private Notification notification;
+        private Data data;
+        private String token;
+    }
+
+    @Builder
+    @AllArgsConstructor
+    @Getter
+    public static class Notification {
+        private String title;
+        private String body;
+        private String image;
+    }
+
+    @Builder
+    @AllArgsConstructor
+    @Getter
+    public static class Data {
+        private String contentType;
+        private String categoryType;
+        private String postId;
+    }
+
+}
--- src/main/java/community/mingle/api/global/firebase/FcmService.java
+++ src/main/java/community/mingle/api/global/firebase/FcmService.java
@@ -1,13 +1,21 @@
 package community.mingle.api.global.firebase;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.MulticastMessage;
 import com.google.firebase.messaging.Notification;
 import community.mingle.api.enums.ContentType;
+import community.mingle.api.infra.SecretsManagerService;
 import lombok.RequiredArgsConstructor;
+import okhttp3.*;
 import org.springframework.stereotype.Component;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 @Component
@@ -16,6 +24,10 @@ public class FcmService {
 
     private static final int BATCH_SIZE = 500;
     private final FirebaseApp firebaseApp;
+    private final ObjectMapper objectMapper;
+    private final SecretsManagerService secretsManagerService;
+    private final String profile;
+    private final String API_URL = "https://fcm.googleapis.com/v1/projects/mingle-new/messages:send"; //dev
 
     public void sendAllMessage(String title, String body, Long contentId, ContentType contentType, List<String> allTokens) {
         int total = allTokens.size();
@@ -26,6 +38,41 @@ public class FcmService {
             FirebaseMessaging.getInstance(firebaseApp).sendMulticastAsync(messages);
             count += BATCH_SIZE;
         }
+    }
+
+    public void sendMessage(String title, String body, Long contentId, ContentType contentType, List<String> allTokens) throws IOException {
+        String message = makeMessage(allTokens.get(0), contentType,  title, body, contentId);
+        OkHttpClient client = new OkHttpClient();
+        RequestBody requestBody = RequestBody.create(message, MediaType.get("application/json; charset=utf-8"));
+        Request request = new Request.Builder().url(API_URL).post(requestBody).addHeader("AUTHORIZATION", "Bearer " + getAccessToken())
+                .addHeader("CONTENT_TYPE", "application/json; UTF-8")
+                .build();
+        Response response = client.newCall(request).execute();
+        System.out.println(response.body().string());
+    }
+
+    private String makeMessage(String targetToken, ContentType contentType, String title, String body, Long postId) throws JsonProcessingException {
+        FcmMessage fcmMessage = FcmMessage.builder()
+                .message(FcmMessage.Message.builder().token(targetToken)
+                        .notification(FcmMessage.Notification.builder().title(title).body(body).image(null).build())
+                        .data(FcmMessage.Data.builder().
+                                contentType(contentType.name()).
+                                postId(String.valueOf(postId)).build())
+                        .build())
+                .validate_only(false).build();
+        return objectMapper.writeValueAsString(fcmMessage);
+    }
+
+    private String getAccessToken() throws IOException {
+        InputStream firebaseToken = convertToStream(secretsManagerService.getFcmToken(profile));
+        GoogleCredentials googleCredentials = GoogleCredentials.fromStream(firebaseToken)
+                .createScoped(List.of("https://www.googleapis.com/auth/cloud-platform"));
+        googleCredentials.refreshIfExpired();
+        return googleCredentials.getAccessToken().getTokenValue();
+    }
+
+    private InputStream convertToStream(String token) {
+        return new ByteArrayInputStream(token.getBytes());
     }
 
     public MulticastMessage buildMessage(String title, String body, Long contentId, ContentType contentType, List<String> fcmTokens) {
