c2333f3d61bdcfdcf4b016f798a6b863ecb4f6b2
HYEONWOO JUNG <leojung0909@gmail.com>
2024-04-09 02:28:35 +0900

    university email domain is nullable

--- src/main/java/community/mingle/api/domain/member/service/UniversityService.java
+++ src/main/java/community/mingle/api/domain/member/service/UniversityService.java
@@ -24,7 +24,14 @@ public class UniversityService {
                 //TODO 하나의 대학에 여러 개의 이메일 도메인이 있을 것을 대비해서 List<String> 으로 프론트에 전달
                 //현재 하나의 대학에 하나의 도메인만 가능한 테이블 구조를 개선할 필요가 있음
                 //아니면 현재 하나의 컬럼에 csv 형태로 저장 후 converter를 이용해 List로 변환
-                .map(university -> new UniversityResponse(university.getId(), university.getName(), List.of(university.getEmailDomain())))
+                .map(university -> {
+                    List<String> universityEmailDomainList;
+                    if (university.getEmailDomain() == null) {
+                        universityEmailDomainList = List.of();
+                    } else universityEmailDomainList = List.of(university.getEmailDomain());
+
+                    return new UniversityResponse(university.getId(), university.getName(), universityEmailDomainList);
+                })
                 .collect(Collectors.toList());
 
     }
