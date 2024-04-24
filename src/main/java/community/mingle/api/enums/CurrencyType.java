c205a9bfcee6e30846bc52df149806cf1f1f8ba4
HYEONWOO JUNG <leojung0909@gmail.com>
2024-04-24 16:27:31 +0900

    fix: currency에 RMB 추가

--- src/main/java/community/mingle/api/enums/CurrencyType.java
+++ src/main/java/community/mingle/api/enums/CurrencyType.java
@@ -12,6 +12,7 @@ public enum CurrencyType {
     KRW(List.of("홍콩", "싱가포르", "영국")),
     HKD(List.of("홍콩")),
     SGD(List.of("싱가포르")),
+    RMB(List.of("중국")),
     GBP(List.of("영국"));
 
     private final List<String> countries;
