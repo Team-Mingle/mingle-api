461b377ab94ac5431f1cb34f4913275656912184
taehyun <tanggu01@connect.hku.hk>
2024-05-20 23:16:42 +0900

    wip: fix MailAuthenticationException error

--- src/main/java/community/mingle/api/configuration/JavaMailSenderConfiguration.java
+++ src/main/java/community/mingle/api/configuration/JavaMailSenderConfiguration.java
@@ -17,11 +17,11 @@ public class JavaMailSenderConfiguration {
         JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
 
         Properties properties = new Properties();
-        properties.put("mail.smtp.auth", false);
+        properties.put("mail.smtp.auth", true);
         properties.put("mail.smtp.starttls.enable", true);
 //        properties.put("mail.smtp.ssl.trust", "smtp-relay.gmail.com"); // SSL 설정 추가
 //        properties.put("mail.smtp.ssl.enable", true); // SSL 활성화
-//        properties.put("mail.debug", true); // 디버그 모드 활성화
+        properties.put("mail.debug", true); // 디버그 모드 활성화
 
         javaMailSender.setHost("smtp-relay.gmail.com");
         javaMailSender.setPort(25);
