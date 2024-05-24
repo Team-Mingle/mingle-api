fcd5ab9fb0f63048ce01d7c6381293353e440e10
taehyun <tanggu01@connect.hku.hk>
2024-05-24 22:04:36 +0900

    fix: JavaMailSender 로컬 해결

--- src/main/java/community/mingle/api/configuration/JavaMailSenderConfiguration.java
+++ src/main/java/community/mingle/api/configuration/JavaMailSenderConfiguration.java
@@ -19,14 +19,14 @@ public class JavaMailSenderConfiguration {
         Properties properties = new Properties();
         properties.put("mail.smtp.auth", true);
         properties.put("mail.smtp.starttls.enable", true);
+        properties.put("mail.smtp.starttls.required", true); // 추가 : https://bootcamptoprod.com/spring-boot-email/
         properties.put("mail.smtp.ssl.trust", "smtp-relay.gmail.com"); // SSL 설정 추가
-//        properties.put("mail.smtp.ssl.enable", true); // SSL 활성화
         properties.put("mail.debug", true); // 디버그 모드 활성화
 
         javaMailSender.setHost("smtp-relay.gmail.com");
-        javaMailSender.setPort(25);
+        javaMailSender.setPort(587);
         javaMailSender.setUsername("no-reply@mingle.community");
-        javaMailSender.setPassword("doybqwvbafqkdbiv");
+        javaMailSender.setPassword("wymbigouckqkpfmm");
         javaMailSender.setJavaMailProperties(properties);
 
         return javaMailSender;
