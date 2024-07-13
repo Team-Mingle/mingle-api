e4a73838fd5c979a1203f8e65b4d2d546df82a7f
taehyun <tanggu01@connect.hku.hk>
2024-07-13 18:12:08 +0900

    feat: 강의 검색 paging 추가

--- src/main/java/community/mingle/api/domain/course/repository/CrawledCourseRepository.java
+++ src/main/java/community/mingle/api/domain/course/repository/CrawledCourseRepository.java
@@ -2,8 +2,11 @@ package community.mingle.api.domain.course.repository;
 
 import community.mingle.api.domain.course.entity.CrawledCourse;
 import community.mingle.api.domain.member.entity.University;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,6 +15,6 @@ import java.util.List;
 public interface CrawledCourseRepository extends JpaRepository<CrawledCourse, Long> {
 
     @Query("SELECT c FROM CrawledCourse c WHERE (c.name LIKE %:keyword% OR c.courseCode LIKE %:keyword%) AND c.university = :university")
-    List<CrawledCourse> findByKeyword(String keyword, University university);
+    Page<CrawledCourse> findByKeyword(@Param("keyword") String keyword, @Param("university") University university, Pageable pageable);
 
 }
